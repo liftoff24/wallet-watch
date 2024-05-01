8e5a2292007f2ef457a69b7df2178c64e42db668
liftoff24 <96268520+liftoff24@users.noreply.github.com>
2024-05-01 18:58:13 +0300

    fourth commit

--- client/src/scenes/fraud-detection/index.jsx
+++ client/src/scenes/fraud-detection/index.jsx
@@ -53,7 +53,7 @@ const FraudDetection = () => {
 
   const makePrediction = async (predictionData) => {
     try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/predict`, {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}predict`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
